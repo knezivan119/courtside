82f1d42a6c46334086a271f0c024e66aa2df951b
Ivan Knezevic <knezivan.au@gmail.com>
2025-08-30 20:26:56 +1000

    Disabled proxy security temporarily - need a bit more work :)

--- public/proxy.php
+++ public/proxy.php
@@ -3,14 +3,14 @@
 $url = "https://www.courtside1891.basketball";
 
 # SECURITY (?)
-$allowedOrigin = $_SERVER['HTTP_HOST'];
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+// $allowedOrigin = $_SERVER['HTTP_HOST'];
+// $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
-if ( $origin !== "https://$allowedOrigin" && $origin !== "http://$allowedOrigin" ) {
-    http_response_code( 403 );
-    echo "Forbidden";
-    exit;
-}
+// if ( $origin !== "https://$allowedOrigin" && $origin !== "http://$allowedOrigin" ) {
+//     http_response_code( 403 );
+//     echo "Forbidden";
+//     exit;
+// }
 
 # FETCH
 $data = @file_get_contents( $url );
